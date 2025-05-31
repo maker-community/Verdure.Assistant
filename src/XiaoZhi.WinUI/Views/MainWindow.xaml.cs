e9a427dc761de98e4c263789553c13a8c957884d
gil zhang <gil.zhang.dev@outlook.com>
2025-05-31 20:28:52 +0800

    add app icon

--- src/XiaoZhi.WinUI/Views/MainWindow.xaml.cs
+++ src/XiaoZhi.WinUI/Views/MainWindow.xaml.cs
@@ -4,23 +4,29 @@ using Microsoft.UI.Xaml.Navigation;
 using System;
 using Microsoft.Extensions.Logging;
 using Microsoft.Windows.ApplicationModel.Resources;
+using WinUIEx;
 
 namespace XiaoZhi.WinUI.Views;
 
 /// <summary>
 /// 主窗口 - Frame导航模式的主窗口
 /// </summary>
-public sealed partial class MainWindow : Window
+public sealed partial class MainWindow : WindowEx
 {
     private readonly ILogger<MainWindow>? _logger;
 
-    private readonly ResourceLoader _resourceLoader =new();
-
+    private readonly ResourceLoader _resourceLoader =new();    
     public MainWindow()
     {
         this.InitializeComponent();
         _logger = App.GetService<ILogger<MainWindow>>();
+
+        // 配置自定义标题栏
+        //this.ExtendsContentIntoTitleBar = true;
+        //this.SetTitleBar(TitleBarGrid);
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/logo.ico"));
         Title = _resourceLoader.GetString("AppDisplayName");
+        
         // 导航到默认页面 (HomePage)
         ContentFrame.Navigate(typeof(HomePage));
     }
